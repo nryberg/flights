0c7fab2d450f37c8fc5226d8a7814ad531bfcba0
Nick Ryberg <nryberg@gmail.com>
2023-07-04 15:32:34 -0500

    Update rsync and jq

---
+++ process_json.sh
@@ -0,0 +1,6 @@
+# Process a series of json files using jq
+# First gather all of the current files into one
+jq -s . ./dump1090-fa/history*.json > dump_1090_history.json
+
+# Then process the file to extract the data we want
+jq '.[] | {now: .now, aircraft: .aircraft[]} | {timestamp: .now, hex: .aircraft.hex, flight: .aircraft.flight, speed: .aircraft.gs, altitude: .aircraft.alt_baro, track: .aircraft.track, vertical_rate: .aircraft.baro_rate, aircraft_category: .aircraft.category, latitude: .aircraft.lat, longitude: .aircraft.lon } ' dump_1090_history.json > dump_1090_history_processed.json
---
+++ rsync_current.sh
@@ -0,0 +1,1 @@
+rsync -a nick@192.168.4.222:/run/dump1090-fa/ ./dump1090-fa/
