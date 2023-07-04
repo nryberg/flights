5a744c97d2028201646ccb13fccda2fe9f707a16
Nick Ryberg <nryberg@gmail.com>
2023-07-04 15:55:21 -0500

    Update process_json.sh
    Change column name

--- process_json.sh
+++ process_json.sh
@@ -3,4 +3,4 @@
 jq -s . ~/dump1090-fa/history*.json > dump_1090_history.json
 
 # Then process the file to extract the data we want
-jq '.[] | {now: .now, aircraft: .aircraft[]} | {ts_ms: .now, hex: .aircraft.hex, flight: .aircraft.flight, speed: .aircraft.gs, altitude: .aircraft.alt_baro, track: .aircraft.track, vertical_rate: .aircraft.baro_rate, aircraft_category: .aircraft.category, latitude: .aircraft.lat, longitude: .aircraft.lon } ' dump_1090_history.json > dump_1090_history_processed.json
+jq '.[] | {now: .now, aircraft: .aircraft[]} | {ts_epoch_seconds: .now, hex: .aircraft.hex, flight: .aircraft.flight, speed: .aircraft.gs, altitude: .aircraft.alt_baro, track: .aircraft.track, vertical_rate: .aircraft.baro_rate, aircraft_category: .aircraft.category, latitude: .aircraft.lat, longitude: .aircraft.lon } ' dump_1090_history.json > dump_1090_history_processed.json
