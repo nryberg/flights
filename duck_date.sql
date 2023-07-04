ae3ccbba50f2f2a3d1fec5f73ed5299d6774f9d1
Nick Ryberg <nryberg@gmail.com>
2023-07-04 16:26:12 -0500

    SQL

--- duck_date.sql
+++ duck_date.sql
@@ -1,10 +1,13 @@
-with fix_ts as
-(select epoch_ms(cast( ts_epoch_seconds * 1000 as bigint)) as timestamp_gmt from 'dump_1090_history_processed.json'),
+with fix_ts as (
+    select epoch_ms(cast(ts_epoch_seconds * 1000 as bigint)) as timestamp_gmt
+    from 'dump_1090_history_processed.json'
+),
 correct_timezone as (
-select timestamp_gmt - INTERVAL 5 HOUR as timestamp_cst
-from fix_ts
+    select timestamp_gmt - INTERVAL 5 HOUR as timestamp_cst
+    from fix_ts
 )
-SELECT timestamp_cst, count(*) as count
+SELECT timestamp_cst,
+    count(*) as count
 from correct_timezone
 group by timestamp_cst
 order by timestamp_cst desc
