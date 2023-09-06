df17584f7d2b0be67c4ceb7d02d4cafd314bd51c
Nick Ryberg <nryberg@gmail.com>
2023-09-05 22:38:07 -0500

    Test tweak

--- load.sh
+++ load.sh
@@ -3,6 +3,7 @@
 ./rsync_current.sh
 ./process_json.sh
 
+
 /home/nick/duckdb ./flights.db < 'duck_clear_dump_1090.sql'
 /home/nick/duckdb ./flights.db < 'duck_load_dump_1090.sql'
 /home/nick/duckdb ./flights.db < 'duck_store_flights.sql'
