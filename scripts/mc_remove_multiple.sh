c5dcbf5f9bf08f0446fa6a6a0df056372396d11d
nryberg <nryberg@gmail.com>
2024-01-25 06:12:21 -0600

    MC Remove

--- scripts/mc_remove_multiple.sh
+++ scripts/mc_remove_multiple.sh
@@ -8,7 +8,9 @@ IMPFILES=(data/*.json)
 for i in ${IMPFILES[@]}
     do
         echo $i
-	mc rm box/flightaware/$i
+        a="$(cut -d'/' -f2 <<<"$i")"
+        echo $a
+      	mc rm littlebox/flights/$a --force --dangerous
         # psql $PG_NEON -c "\\copy flight_landing from '$i' DELIMITER ',' CSV HEADER"
     done
 
