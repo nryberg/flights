5f31ab63e7090af05837cf0be51eac656936c5fa
nryberg <nryberg@gmail.com>
2024-01-25 15:38:46 -0600

    Next swag

--- scripts/mc_pull_range.sh
+++ scripts/mc_pull_range.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-pattern="16951*.json"
-outdir="/home/nick/develop/GitHub/nryberg/flights/data"  # littlebox
+pattern="16952*.json"
 outdir="/Users/nick/Documents/GitHub/nryberg/flights/data"  # Mac
+outdir="/home/nick/develop/GitHub/nryberg/flights/data"  # littlebox
 
 mc find littlebox/flights --name $pattern --exec "mc cp {} $outdir"
