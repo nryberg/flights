7fc117a057de737b83a4e24b9a5de1fdf1b8dea7
nryberg <nryberg@gmail.com>
2024-01-29 20:56:59 -0600

    Update psq_test_airframes.sql

--- sql/psq_test_airframes.sql
+++ sql/psq_test_airframes.sql
@@ -1,58 +1,86 @@
-select 
-*
-from airframes
-limit 10
+select
+    *
+from
+    airframes
+limit
+    10
+delete from
+    airframes
+where
+    icao24 = 'ica024';
 
-
-delete from airframes
-where icao24 = 'ica024';
-
-SELECT count(distinct(hex)) as counter 
-from flights fl 
-left JOIN airframes af on af.icao24 = fl.hex
-where af.icao24 is not null
-LIMIT 10
-
-SELECT distinct af.*
-from airframes af
--- TABLESAMPLE BERNOULLI (10)
-JOIN flights fl on fl.hex = af.icao24
--- LIMIT 100;
-
---- Airframes 
-SELECT distinct icao24,
-registration,
--- manufacturericao,
-manufacturername,
-model,
-typecode,
-operator,
-operatorcallsign,
-operatoricao,
-owner_name,
-built
-FROM airframes af
--- TABLESAMPLE BERNOULLI (10)
-JOIN flights fl on fl.hex = af.icao24
-WHERE operatorcallsign is not null
-and operatorcallsign <> ''
-and typecode <> ''
-AND operator <> ''
-oRDER by operatorcallsign
-
---- Flights 
-with frames as (
-SELECT distinct icao24
-FROM airframes af
-JOIN flights fl on fl.hex = af.icao24
-WHERE operatorcallsign is not null
-and operatorcallsign <> ''
-and typecode <> ''
-AND operator <> ''
-)
-SELECT *
-FROM flights fl
-join frames fr 
-on fl.hex = fr.icao24
-
--- LIMIT 10;
+SELECT
+    count(distinct(hex)) as counter
+from
+    flights fl
+    left JOIN airframes af on af.icao24 = fl.hex
+where
+    af.icao24 is not null
+LIMIT
+    10
+SELECT
+    distinct af.*
+from
+    airframes af -- TABLESAMPLE BERNOULLI (10)
+    JOIN flights fl on fl.hex = af.icao24 -- LIMIT 100;
+    --- Airframes 
+SELECT
+    distinct icao24,
+    registration,
+    -- manufacturericao,
+    manufacturername,
+    model,
+    typecode,
+    operator,
+    operatorcallsign,
+    operatoricao,
+    owner_name,
+    built
+FROM
+    airframes af -- TABLESAMPLE BERNOULLI (10)
+    JOIN flights fl on fl.hex = af.icao24
+WHERE
+    operatorcallsign is not null
+    and operatorcallsign <> ''
+    and typecode <> ''
+    AND operator <> ''
+oRDER by
+    operatorcallsign 
+    
+    --- Flights 
+    with frames as (
+        SELECT
+            distinct icao24
+        FROM
+            airframes af
+            JOIN flights fl on fl.hex = af.icao24
+        WHERE
+            operatorcallsign is not null
+            and operatorcallsign <> ''
+            and typecode <> ''
+            AND operator <> ''
+    ),
+    flights as (
+        SELECT
+            fl.*,
+            substring(ts_iso8601, 1, 10) as ts_date
+        FROM
+            flights fl
+            join frames fr on fl.hex = fr.icao24
+        where
+            alt > 30000
+            and groundspeed > 0
+    )
+SELECT
+    max(ts_epoch),
+    hex,
+    ts_date, 
+    alt,
+    groundspeed
+    track
+from
+    flights  fl
+    join frames fr on fl.hex = fr.icao24
+GROUP BY
+    hex,
+    ts_date -- LIMIT 10;
